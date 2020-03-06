83471a0b6afb4de1f603b7b824f40bdb9460aa42
straslerj <straslerj@msoe.edu>
2020-03-05 21:01:59 -0500

    Cleaned up code according to MSOE checkstyle; added JavaDocs

--- src/straslerj/TipCalculatorController.java
+++ src/straslerj/TipCalculatorController.java
@@ -1,6 +1,10 @@
+/*
+ * Name: Jake Strasler
+ * Tip Calculator
+ * Created: 3/3/2020
+ */
 package straslerj;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -9,6 +13,9 @@ import javafx.scene.control.TextField;
 import java.text.NumberFormat;
 import java.util.Locale;
 
+/**
+ * Controller class for TipCalculator; contains the project's logic
+ */
 public class TipCalculatorController {
     private static final double FIFTEEN_PERCENT_TIP = 0.15;
     private static final double TWENTY_PERCENT_TIP = 0.20;
@@ -48,58 +55,82 @@ public class TipCalculatorController {
     @FXML
     Label newTotalCalculated;
 
+    /**
+     * Gets the initial total entered by the user
+     * @return total entered by user as a double
+     */
     public double getTotal() {
         total = Double.parseDouble(totalTextField.getText());
         return total;
     }
 
-    public void setTotalCalculatedFifteen(ActionEvent event) {
+    /**
+     * Determines the total and tip when a user selects 15%
+     */
+    public void setTotalCalculatedFifteen() {
         getTotal();
         tipCalculated.setText(String.valueOf(n.format(total * FIFTEEN_PERCENT_TIP)));
         totalCalculated.setText(String.valueOf(n.format(total + (total * FIFTEEN_PERCENT_TIP))));
     }
 
-    public void setTotalCalculatedTwenty(ActionEvent event) {
+    /**
+     * Determines the total and tip when a user selects 20%
+     */
+    public void setTotalCalculatedTwenty() {
         getTotal();
         tipCalculated.setText(String.valueOf(n.format(total * TWENTY_PERCENT_TIP)));
         totalCalculated.setText(String.valueOf(n.format(total + (total * TWENTY_PERCENT_TIP))));
     }
 
-    public void setTotalCalculatedTwentyfive(ActionEvent event) {
+    /**
+     * Determines the total and tip when a user selects 25%
+     */
+    public void setTotalCalculatedTwentyfive() {
         getTotal();
         tipCalculated.setText(String.valueOf(n.format(total * TWENTYFIVE_PERCENT_TIP)));
         totalCalculated.setText(String.valueOf(n.format(total + (total * TWENTYFIVE_PERCENT_TIP))));
     }
 
-    public void roundUp(ActionEvent event) {
+    /**
+     * Rounds the dollar value up to the nearest whole dollar,
+     * adjusts the tip dollar value and percentage accordingly
+     */
+    public void roundUp() {
         double greaterThanCheck = Double.parseDouble(totalCalculated.getText().substring(1));
         double roundedUp = Math.round(Double.parseDouble((totalCalculated.getText().substring(1))));
         if((greaterThanCheck - (Math.round(greaterThanCheck))) > 0) {
             newTotalCalculated.setText(String.valueOf(n.format(Math.round(Double.parseDouble(
                     (totalCalculated.getText().substring(1)))) + 1.00)));
-            roundedUp = Math.round(Double.parseDouble((totalCalculated.getText().substring(1)))) + 1.00;
+            roundedUp = Math.round(Double.parseDouble(
+                    (totalCalculated.getText().substring(1)))) + 1.00;
         } else {
-            newTotalCalculated.setText
-                    (n.format(Math.round(Double.parseDouble((totalCalculated.getText().substring(1))))));
+            newTotalCalculated.setText(n.format(
+                    Math.round(Double.parseDouble((totalCalculated.getText().substring(1))))));
         }
         double roundedTip = roundedUp - total;
-        newTipCalculated.setText(n.format(roundedTip) + " (" + format.format(roundedTip / total) + ")");
+        newTipCalculated.setText(n.format(roundedTip) + " (" + format.format(
+                roundedTip / total) + ")");
     }
 
-    public void roundDown(ActionEvent event) {
+    /**
+     * Rounds the dollar value down to the nearest whole dollar,
+     * adjusts the tip dollar value and percentage accordingly
+     */
+    public void roundDown() {
         double greaterThanCheck = Double.parseDouble(totalCalculated.getText().substring(1));
-        double roundedDown = Math.round(Double.parseDouble((totalCalculated.getText().substring(1))));
+        double roundedDown = Math.round(Double.parseDouble(
+                (totalCalculated.getText().substring(1))));
         if((greaterThanCheck - (Math.round(greaterThanCheck))) > 0) {
-            newTotalCalculated.setText(String.valueOf(n.format(Math.round(Double.parseDouble(
-                    (totalCalculated.getText().substring(1)))))));
+            newTotalCalculated.setText(n.format(Math.round(Double.parseDouble(
+                    (totalCalculated.getText().substring(1))))));
         } else {
-            newTotalCalculated.setText
-                    (n.format(Math.round(Double.parseDouble((totalCalculated.getText().substring(1))) - 1.00)));
-            roundedDown = Math.round(Double.parseDouble((totalCalculated.getText().substring(1))) - 1.00);
+            newTotalCalculated.setText(n.format(Math.round(
+                    Double.parseDouble((totalCalculated.getText().substring(1))) - 1.00)));
+            roundedDown = Math.round(Double.parseDouble(
+                    (totalCalculated.getText().substring(1))) - 1.00);
         }
-        //newTotalCalculated.setText(n.format(Math.round(Double.parseDouble((totalCalculated.getText().substring(1))))));
         double roundedTip = roundedDown -total;
-        newTipCalculated.setText(n.format(roundedTip) + " (" + format.format(roundedTip / total) + ")");
+        newTipCalculated.setText(n.format(roundedTip) + " (" + format.format(
+                roundedTip / total) + ")");
     }
-
 }
--- src/straslerj/TipCalculatorDriver.java
+++ src/straslerj/TipCalculatorDriver.java
@@ -1,3 +1,8 @@
+/*
+ * Name: Jake Strasler
+ * Tip Calculator
+ * Created: 3/3/2020
+ */
 package straslerj;
 
 import javafx.application.Application;
@@ -6,13 +11,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Driver class for TipCalculator project; required in all JavaFX projects
+ */
 public class TipCalculatorDriver extends Application {
+    private static final int WIDTH = 300;
+    private static final int HEIGHT = 275;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("TipCalculator.fxml"));
         primaryStage.setTitle("Tip Calculator");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, WIDTH, HEIGHT));
         primaryStage.show();
     }
 
